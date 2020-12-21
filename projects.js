84643713e3633d035252e74e2e6442cb3c15261c
volovatz <51710283+volovatz@users.noreply.github.com>
2020-12-21 14:55:20 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 const projects={
 	'M.N.K APARTMENT': {cnt: 11, txt: 'SHOHAM 2019 | Photography by Giddon Levin'},
 	'V.R.A APARTMENT': {cnt: 4, txt: 'Bat Yam 2018 | Photography by Adva Raz'},
-        'K.R.R APARTMENT': {cnt: 1, txt: ''},
+        'K.R.R APARTMENT': {cnt: 0, txt: ''},
 	
 	}
